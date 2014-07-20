4fcec5ba3aeec796ae747d96a822cb073c5c6292
mtekasetya <mtekasetya@me.com>
2014-07-20 18:37:49 -0500

    initial commit

---
+++ AngularPost/App/js/app.js
@@ -0,0 +1,39 @@
+﻿var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider, $locationProvider) {
+
+    $routeProvider
+        .when('/Home/', {
+            templateUrl: '/Templates/customer.html',
+            controller: 'customerCtrl'
+        })
+        .otherwise({
+            redirectTo: '/Home/'
+        });
+
+    $locationProvider.html5Mode(true);
+
+});
+
+myApp.controller('customerCtrl', function ($scope, customerSvc) {
+
+    $scope.save1 = function (customer) {
+        customerSvc.save1(customer);
+    };
+
+    $scope.save2 = function (customer) {
+        customerSvc.save2(customer);
+    };
+
+});
+
+myApp.factory('customerSvc', function ($http) {
+    return {
+        save1: function(customer) {
+            $http.post('/Home/Save1', customer); // CustomerVm
+        },
+        save2: function (customer) {
+            $http.post('/Home/Save2', customer); // FormCollection
+        }
+    };
+});
---
+++ AngularPost/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularPost.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Save1(CustomerVm customer)
+        {
+            //TODO: Do some stuffs...
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        public ActionResult Save2(FormCollection customer)
+        {
+            //TODO: Do some stuffs...
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+    }
+
+    public class CustomerVm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
